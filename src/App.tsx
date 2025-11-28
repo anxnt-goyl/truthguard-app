5c9d8c7f96d041e80786ff27d456e7ae4fe7cf55
anxnt-goyl <anantg0609@gmail.com>
2025-11-28 23:38:21 +0530

    Initial commit - TruthGuardApp project

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TruthGuardApp from "./components/truthguardapp"; // Your login page
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      {/* Global notification toasters */}
+      <Toaster />
+      <Sonner />
+
+      <BrowserRouter>
+        <Routes>
+          {/* Main login page */}
+          <Route path="/" element={<TruthGuardApp />} />
+
+          {/* Other pages */}
+          <Route path="/index" element={<Index />} />
+
+          {/* Catch-all for 404 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
